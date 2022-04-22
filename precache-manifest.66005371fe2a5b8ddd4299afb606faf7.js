ee0acc755d34bae9d079f6ce242d18f98f4dea6a
shivam mishra <ShivamMishra828>
2022-04-22 10:08:04 +0530

    Updates

---
+++ precache-manifest.66005371fe2a5b8ddd4299afb606faf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a788cae69fe26131985b13ddda201429",
+    "url": "/Translate-React-App/index.html"
+  },
+  {
+    "revision": "1b706cff84a1ba3da3b9",
+    "url": "/Translate-React-App/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "212f1f38b0d994a37ab3",
+    "url": "/Translate-React-App/static/js/2.d3633cdd.chunk.js"
+  },
+  {
+    "revision": "1b706cff84a1ba3da3b9",
+    "url": "/Translate-React-App/static/js/main.2eadf78f.chunk.js"
+  },
+  {
+    "revision": "1af7ac441c82fbd5c5e4",
+    "url": "/Translate-React-App/static/js/runtime~main.2d31e6b1.js"
+  }
+]);
